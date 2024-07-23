d9e709162134cd9a170e0a1ba9ae110209b2f818
gabriellandgraf <gabriellandgrafdecarvalho54@gmail.com>
2024-07-23 17:23:57 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+
+<body>
+
+    <?php
+   $Biologia=80;
+   $Matematica=90;
+   $Fisica=70;
+   $Historia=75;
+   $Educação_fisica=100;
+   $Educação_financeira=80;
+   $Sociologia=65;
+   $Geografia=90;
+   $Portugues=80;
+   $Quimica=60;
+
+
+   $media=79;
+
+
+   echo 'Media 79 aprovado'
+    ?>
+
+</body>
+
+</html>
